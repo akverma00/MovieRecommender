323f404172b6932943d40625f9de3a561b97f4e1
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2020-06-16 05:19:50 +0530

    Get Related Movies

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def get_movies_from_tastedive(word):
     dic['limit'] = 5
     return requests_with_caching.get(baseurl, params=dic).json()
 
+
+def extract_movie_titles(word):
+    return [x["Name"] for x in word["Similar"]["Results"]]
+
+
+def get_related_titles(movies):
+    l2 = []
+    for movie in movies:
+        l2 = l2 + [title for title in extract_movie_titles(get_movies_from_tastedive(movie)) if title not in l2]
+    return l2
+
+
+def get_movie_data(movie):
+    baseurl = "http://www.omdbapi.com/"
+    dic = {}
+    dic['t'] = movie
+    dic['r'] = 'json'
+    return requests_with_caching.get(baseurl, params=dic).json()
